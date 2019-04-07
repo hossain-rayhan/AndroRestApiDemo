633319db5694ea4b4a9a1071325587c9f73374a2
Rayhan Hossain <hossain.rayhan@outlook.com>
2019-04-06 22:00:01 -0400

    get data using Retrofit client from the given REST API

--- app/src/main/java/com/rayapplica/androrestapidemo/MainActivity.java
+++ app/src/main/java/com/rayapplica/androrestapidemo/MainActivity.java
@@ -1,15 +1,30 @@
 package com.rayapplica.androrestapidemo;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.rayapplica.androrestapidemo.model.Data;
+import com.rayapplica.androrestapidemo.model.User;
+import com.rayapplica.androrestapidemo.network.RetrofitClient;
+import com.rayapplica.androrestapidemo.network.UserDataService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+    UserDataService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +41,40 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+        service = RetrofitClient.getRetrofitInstance().create(UserDataService.class);
+        getUserData();
     }
 
+    private void getUserData(){
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("...Loading...");
+        progressDialog.show();
+        
+        Call<Data> call = service.getData();
+        call.enqueue(new Callback<Data>() {
+            @Override
+            public void onResponse(Call<Data> call, Response<Data> response) {
+                progressDialog.dismiss();
+                generateDataList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<Data> call, Throwable t) {
+                Log.e("On Failor Error", t.getMessage());
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void generateDataList(Data data){
+        for (User user : data.getUserList()){
+            Log.e("User", user.getFirstName());
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
